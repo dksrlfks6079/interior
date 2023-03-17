83f6b3e6fc5a06a599454ef86393dc0d6309393f
Akr <dksrlfks6079@naver.com>
2023-03-17 17:21:57 +0900

    interior

--- js/slide.js
+++
@@ -1,7 +0,0 @@
-const contents = document.querySelectorAll(".btn")
-
-contents.forEach(item => {
-    item.addEventListener("click", function () {
-        this.classList.toggle("active")
-    })
-})
